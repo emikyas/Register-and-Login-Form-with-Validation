9965a30d87b1fef9bef1574d8f2f30ad53be0efb
Jurjani Valles <jurjani.valles@gmail.com>
2021-06-07 17:15:48 +0800

    Added Login Form and Register Form

---
+++ src/component/pages/loginForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>w</h1> );
+    }
+}
+ 
+export default LoginForm;
